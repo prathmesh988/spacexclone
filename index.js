6dcbff3f447c8ae1be02d0b5bc1fde46f278fc9c
prathmesh988 <prathmeshgawade41@gmail.com>
2023-08-02 12:39:59 +0530

    the 4th commit

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
 console.log('hello world');
+function print(data){
+    console.log(data);
+}
+
+const body=document.querySelector('body');
 const a = document.getElementById('mnuepage');
 const b = document.getElementById('button1');
 const c = document.getElementById('butt2');
 const lis = document.querySelectorAll('.whomeliiii')
 const ul = document.querySelector('#flex-item2');
 const khalibox =  document.querySelector('#flex-item1');
-const hr = document.querySelector('hr');
+const hr = document.querySelectorAll('hr');
 b.addEventListener('click',()=>{
     // console.log(a.childNodes[3]);
     // console.log(lis);
-
-    a.style.display='inline-flex';
-
-    a.style.flexDirection='row-reverse';
+    
+    a.style.display='flex';
+    
+    a.style.flexDirection='row';
     khalibox.style.display = 'block';
     ul.style.display = 'block';
     for(let i=0; i<lis.length; i++) {
         lis[i].style.display = 'list-item';
     }
-    hr.style.display = 'block';
-    khalibox.style.flexGrow='2.5';
+    for(let z=0; z<hr.length; z++) {
+        hr[z].style.display = 'block';
+    };
+    print(khalibox.classList);
+    ul.setAttribute('class','flex-item2cls');
+    khalibox.setAttribute('class' , 'flex-item1cls')// ---> sets the attribute value that you specified attribute in the function
+
     ul.style.flexGrow = '2';
     ul.style.backgroundColor = 'black';
     a.style.position='absolute';
     a.style.zIndex = '9999';
-    a.style.top = '5vh';
-    a.style.right = '5vh';
+    a.style.top = '0vh';
+    a.style.right = '-0.2vh';
     
     
 })
-c.addEventListener('click',()=>{
-     ul.style.display = 'none  ';
-  for(let i=0; i<lis.length; i++) {
-    lis[i].style.display = 'none';
-  }
-    a.style.display='none';
 
+c.addEventListener('click',()=>{
+    ul.style.display = 'none  ';
+    for(let i=0; i<lis.length; i++) {
+        lis[i].style.display = 'none';
+    }
+    a.style.display='none';
+    
 });
+khalibox.addEventListener('click',()=>{
+    khalibox.style.display = 'none';
+    ul.style.display = 'none  ';
+    for(let i=0; i<lis.length; i++) {
+        lis[i].style.display = 'none';
+    }
+    a.style.display='none';
+   
+})
+
+// the callback's not called when the scroll event is fired
+khalibox.addEventListener('scroll',()=>{
+    khalibox.style.display = 'none';
+    ul.style.display = 'none  ';
+    for(let i=0; i<lis.length; i++) {
+        lis[i].style.display = 'none';
+    }
+    a.style.display='none';
+   
+})
+
+//addEventListener arttaches the callback and its event to the WEBapi  or callbackqueue evnrioment and then  when the event get's fired then the function pushed into the call stack
